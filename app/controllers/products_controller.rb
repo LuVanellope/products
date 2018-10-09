07b135ec755ace6830259117e91a82dbc7ac1c86
Lu <luacosta.fc@gmail.com>
2018-10-08 22:44:03 -0500

    cambio en codigo checkBox

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+
     if @product.save
       redirect_to products_path, notice: "El producto fue publicado con éxito"
     else
--- db/migrate/20181008203845_create_product_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateProductCategories < ActiveRecord::Migration[5.2]
-  def change
-    create_table :product_categories do |t|
-
-      t.timestamps
-    end
-  end
-end
