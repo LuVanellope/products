906e19cd3c24c516890cda8c9289b1e93ac62732
Lu <luacosta.fc@gmail.com>
2018-10-08 23:42:52 -0500

    Elimina la etiqueta Form no necesaria

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_203845) do
+ActiveRecord::Schema.define(version: 2018_10_09_041914) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
